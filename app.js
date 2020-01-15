9080df383ea3d9c2d7179e5de24757bea0167805
Igor <igoreklida@gmail.com>
2020-01-16 00:24:58 +0300

    all info about user in modal

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const nameContainer = document.querySelector('.list-group');
+const modalBody = document.querySelector('#modal-user-info .modal-body');
 let users;
 
 xhrRequest(generateUserName);
+nameContainer.addEventListener('click', listGroupHandler);
 
 function xhrRequest(cbResponse) {
     const xhr = new XMLHttpRequest();
@@ -24,5 +26,33 @@ function generateUserName(arr) {
         li.dataset.toggle = 'modal';
         li.dataset.target = '#modal-user-info';
         nameContainer.append(li);
+    });
+}
+
+function listGroupHandler(e) {
+    const userName = e.target.innerText;
+    const user = users.find((item) => {
+        return userName === item.username;
+    });
+    const userInfo = document.createElement('ul');
+    userInfo.classList.add('list-group', 'list-group-flush');
+    generateAllInfo(user, userInfo);
+    modalBody.innerHTML = '';
+    modalBody.append(userInfo);
+}
+
+function generateAllInfo(obj, parent) {
+    Object.entries(obj).forEach(([key, value]) => {
+        const userInfoElement = document.createElement('li');
+        userInfoElement.classList.add('list-group-item');
+        if(typeof value !== 'object') {
+            userInfoElement.innerHTML = `<strong>${key}</strong> - ${value}`;
+        } else {
+            userInfoElement.innerHTML = `<strong>${key}</strong>`;
+        }
+        parent.append(userInfoElement);
+        if(typeof value === 'object') {
+            generateAllInfo(value, userInfoElement);
+        }
     });
 }
